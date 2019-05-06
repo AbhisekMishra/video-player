fc368ba22f6169231542c57d2b567f0384fed6d1
abhisek.mishra <abhisek.mishra@xebia.com>
2019-05-06 22:35:35 +0400

    added search bar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header';
+import SearchBar from './components/search-bar';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      link: '',
+    };
+  }
+  setLink = link => this.setState({link});
+  render() {
+    const { link } = this.state;
+    return (
+      <div className="App">
+        <Header />
+        <SearchBar link={link} setLink={this.setLink} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/search-bar/search-bar.js
+++ src/components/search-bar/search-bar.js
@@ -1,5 +1,58 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
-const SearchBar = props => <h1>SearchBar</h1>;
+const styles = theme => ({
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: '100%',
+  },
+  header: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  button: {
+    margin: theme.spacing.unit,
+  },
+});
 
-export default SearchBar;
+const SearchBar = (props) => {
+  const {classes, link, setLink, launchVideo} = props;
+  return (
+    <div className={classes.root}>
+      
+          <Grid container justify="center" alignItems="flex-end" spacing={16}>
+            <Grid item xs={6} sm={6} md={3}>
+              <h3 className={classes.header}>Enter video URL</h3>
+              <TextField
+                id="standard-search"
+                label="Link"
+                type="search"
+                className={classes.textField}
+                margin="normal"
+                value={link}
+                onChange={e => setLink(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={4} sm={4} md={1}>
+              <Button variant="contained" color="primary" className={classes.button} onClick={() => launchVideo(link)}>
+                Go
+              </Button>
+            </Grid>
+          </Grid>
+    </div>
+  );
+};
+
+SearchBar.propTypes = {
+  classes: PropTypes.shape({}).isRequired,
+  user: PropTypes.string.isRequired,
+  setUser: PropTypes.func.isRequired,
+  searchGistByUser: PropTypes.func.isRequired,
+};
+
+export default withStyles(styles)(SearchBar);
